2e970a57a0bb3502e33c7017c63390a9f53d73ea
jeffbai996 <j.bai@wustl.edu>
2022-10-17 14:24:17 -0700

    Completed project shell script

--- salon.sh
+++ salon.sh
@@ -13,7 +13,6 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
-
   AVAILABLE_SERVICES=$($PSQL "SELECT * FROM services")
 
   echo -e "Welcome to My Salon, how can I help you?\n"
@@ -32,6 +31,7 @@ MAIN_MENU() {
     read CUSTOMER_PHONE
 
     CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
     # if no customer name
     if [[ -z $CUSTOMER_NAME ]]
     then
@@ -40,12 +40,18 @@ MAIN_MENU() {
 
       # insert new customer
       INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
     fi
     # get appointment time
-    echo -e "\nWhat time would you like your cut,$CUSTOMER_NAME?"
+    echo -e "\nWhat time would you like your cut, "$CUSTOMER_NAME"?"
     read SERVICE_TIME
 
+    # insert new appointment
+    INSERT_APPT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME')")
 
+    # get name of selected service
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+    echo "I have put you down for a"$SERVICE_NAME" at "$SERVICE_TIME","$CUSTOMER_NAME"."
   fi
 }
 
